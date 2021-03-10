48b3a8ac40de7c4bce2e90242ccab5270afb0836
Michael Hueschen <m@mhueschen.space>
2021-03-10 14:58:14 -0600

    pub-ify structs & fns

--- src/toplevel.rs
+++ src/toplevel.rs
@@ -5,7 +5,7 @@ use super::{
     syntax, types, eval, parse::program, env::Env, infer::infer_program};
 
 /// throws an error if the document doesn’t pass type checking
-fn parse_calculation(dsl_document: String) -> Result<syntax::Program, String> {
+pub fn parse_calculation(dsl_document: String) -> Result<syntax::Program, String> {
     match program().parse(easy::Stream(&dsl_document[..])) {
         // TODO this is janky - perhaps we can just return the error?
         // https://docs.rs/combine/4.5.2/combine/trait.StreamOnce.html#associatedtype.Error
@@ -17,19 +17,19 @@ fn parse_calculation(dsl_document: String) -> Result<syntax::Program, String> {
 /// return the "scheme" of the body of a program. this may have free type variables in it.
 /// the `Type` contained within can be fed to `type_arguments` & `type_return`.
 // types::Type::TRelated(op, Type, Type) needs to be implemented to support calculated / derived units
-fn get_calculation_type(program: syntax::Program) -> Result<types::Scheme, String> {
+pub fn get_calculation_type(program: syntax::Program) -> Result<types::Scheme, String> {
     match infer_program(Env::new(), &program) {
         Ok((sc, _env)) => Ok(sc),
         Err(err) => Err(format!("type error: {:?}", err)),
     }
 }
 
-struct ReputationCalculationResult {
-    rcr_calculation: syntax::Expr,
-    ty: types::Type, // type is a reserved keyword of Rust
-    value: eval::Value,
+pub struct ReputationCalculationResult {
+    pub rcr_calculation: syntax::Expr,
+    pub ty: types::Type, // type is a reserved keyword of Rust
+    pub value: eval::Value,
 }
 
-fn reduce_calculation<R>(calculation: syntax::Expr, input_data: &dyn Iterator<Item = eval::Value>) -> ReputationCalculationResult {
+pub fn reduce_calculation<R>(calculation: syntax::Expr, input_data: &dyn Iterator<Item = eval::Value>) -> ReputationCalculationResult {
     todo!()
 }
