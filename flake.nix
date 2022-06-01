7b90452c982e404ae2d5b6acecf21ad519f84481
Michael Hueschen <m@mhueschen.space>
2022-05-31 20:53:14 -0600

    fix flake.nix usage of pkgs; comment out broken derivations
    also do the new `devShells.default` thing.

--- flake.nix
+++ flake.nix
@@ -12,37 +12,37 @@
     in
     flake-utils.lib.eachSystem nh-supported-systems (system:
       let
-        pkgs = nh-nix-env.values.pkgs;
+        pkgs = nh-nix-env.values.${system}.pkgs;
       in
       {
-        devShell = nh-nix-env.shells.${system}.rustDevShell {};
+        devShells.default = nh-nix-env.shells.${system}.rustDevShell {};
 
         # warning: this is broken due to
         # https://github.com/nix-community/naersk/issues/133
-        packages.rep_lang_runtime =
-          let
-            rust = pkgs.rust-bin.stable.${rustVersion}.default;
-
-            naersk' = pkgs.callPackage naersk {
-              cargo = rust;
-              rustc = rust;
-            };
-          in
-          naersk'.buildPackage {
-            src = ./rep_lang_runtime;
-            copyLibs = true;
-          };
+        # packages.rep_lang_runtime =
+        #   let
+        #     rust = pkgs.rust-bin.stable.${rustVersion}.default;
+        #
+        #     naersk' = pkgs.callPackage naersk {
+        #       cargo = rust;
+        #       rustc = rust;
+        #     };
+        #   in
+        #   naersk'.buildPackage {
+        #     src = ./rep_lang_runtime;
+        #     copyLibs = true;
+        #   };
 
         # warning: this is broken due to
         # https://github.com/cargo2nix/cargo2nix/issues/211
-        packages.rep_lang_runtime-cargo2nix =
-          let
-            rustPkgs = pkgs.rustBuilder.makePackageSet' {
-              rustChannel = rustVersion;
-              packageFun = import ./rep_lang_runtime/Cargo.nix;
-            };
-          in
-          rustPkgs.workspace.rep_lang_runtime {};
+        # packages.rep_lang_runtime-cargo2nix =
+        #   let
+        #     rustPkgs = pkgs.rustBuilder.makePackageSet {
+        #       rustChannel = rustVersion;
+        #       packageFun = import ./rep_lang_runtime/Cargo.nix;
+        #     };
+        #   in
+        #   rustPkgs.workspace.rep_lang_runtime {};
 
       });
 }
